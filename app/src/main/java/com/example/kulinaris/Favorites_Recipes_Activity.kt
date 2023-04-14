bc9ba31d9df59f9058b3b92bd98b3f870d050093
Danil Kuzychenko <lymaaaan@gmail.com>
2023-04-14 14:06:55 +0400

    Feat: Favorite Recipes Activity was added

---
+++ app/src/main/java/com/example/kulinaris/Favorites_Recipes_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.kulinaris
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Favorites_Recipes_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites_recipes)
+    }
+}
